42c563717898d0b42592b940a3c9fbea2002820e
pdogn <phamdong9139@gmail.com>
2023-12-15 17:41:45 +0700

    F G cm

---
+++ Assets/Data/MenuScripts/MainMenu.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+    public void StartGame()
+    {
+        SceneManager.LoadSceneAsync(1);
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+
+    private void Update()
+    {
+        AudioManager.instance.SFXRun.Stop();
+    }
+}
